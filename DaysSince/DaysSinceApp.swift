7b1923b4e702ce8999e62b39feda3ab03018cb90
zaid ragab <zragab@lcs.on.ca>
2023-05-25 14:16:23 -0400

    Initial Commit

---
+++ DaysSince/DaysSinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSinceApp.swift
+//  DaysSince
+//
+//  Created by Zaid Ragab on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct DaysSinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
